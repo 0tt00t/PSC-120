479fb62f92b8ee69fe238b7acffef20a787ea51d
0tt00t <82366306+0tt00t@users.noreply.github.com>
2023-10-23 17:36:13 -0700

    Add files via upload

---
+++ freezingAggregation/src/freezingAggregation/GUIfreezing.java
@@ -0,0 +1,20 @@
+package freezingAggregation;
+
+import java.awt.Color;
+
+import spaces.Spaces;
+import sweep.GUIStateSweep;
+import sweep.SimStateSweep;
+
+public class GUIfreezing extends GUIStateSweep {
+
+	public GUIfreezing(SimStateSweep state, int gridWidth, int gridHeight, Color backdrop, Color agentDefaultColor,
+			boolean agentPortrayal) {
+		super(state, gridWidth, gridHeight, backdrop, agentDefaultColor, agentPortrayal);
+		
+	}
+	
+	public static void main(String[] args) {
+		GUIfreezing.initialize(Environment.class, null, GUIfreezing.class, 600, 600, Color.WHITE, Color.BLUE, true, Spaces.SPARSE);
+	}
+}
